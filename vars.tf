9bc5b722598de050ed30893586c898825e72dab5
kpatel <kpatel@savanainc.com>
2021-10-25 22:49:55 -0400

    bug fixed

--- vars.tf
+++ vars.tf
@@ -1,6 +1,4 @@
-variable "env" {
-  default = "tf"
-}
+#Vpc
 variable "vpcCIDRblock"{  
   default = "192.168.0.0/16"
 }
@@ -11,5 +9,21 @@ variable "availabilityZone" {
   default = "us-east-1a"
 }
 variable "vpc-tag" {
-  default = "tfvpc"
+  default= "terraform-vpc"
+}
+
+#public subnet
+variable "public-subnet-cidr"{  
+  default = "192.168.1.0/24"
+}
+variable "public-subnetcidr-tag" {
+  default= "terraform-public"
+}
+
+#private subnet
+variable "private-subnet-cidr" {  
+  default = "192.168.2.0/24"
+}
+variable "private-subnetcidr-tag" {
+  default= "terraform-private"
 }
